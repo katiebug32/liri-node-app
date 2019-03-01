1e94f8bc777587e65ca97b4be9bb7b8963dd6eaa
katiebug32 <katiebug32@gmail.com>
2019-03-01 11:54:16 -0600

    working files

--- liri.js
+++ liri.js
@@ -1,59 +1,186 @@
 
 // At the top of the liri.js file, add code to read and set any environment variables with the dotenv package:
 require("dotenv").config();
-const axios = require("axios");
-const moment = require("moment");
-
-// const Spotify = require("node-spotify-api");
-// var spotify = new Spotify(keys.spotify);
-
-const keys = require("./keys.js");
-
-
-// Add the code required to import the keys.js file and store it in a variable.
-let userInput = process.argv[2];
-let userSearch = process.argv;
-let userSearchVal = process.argv.slice(3).join("+");
-
 
 // Make it so liri.js can take in one of the following commands:
-// concert-this
-
+// concert-this ---- DONE
 // spotify-this-song
 // movie-this
 // do-what-it-says
 
+/////////////////////////////////////////////////
+
+const axios = require("axios");
+const moment = require("moment");
+const keys = require("./keys.js");
+const fs = require('fs');
+
+const Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
+
+let userInput = process.argv[2];
+let userSearchVal = process.argv.slice(3).join(" ");
+
+
+//////////////////////////////////////////////////
+
+
 // node liri.js concert-this <artist/band name here>
+// - Name of the venue
+// - Venue location
+// - Date of the Event (use moment to format this as "MM/DD/YYYY")
+
 const bandsInTownSearch = (artist) => {
     let concertQueryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-    console.log("concert URL: " + concertQueryURL);
-    axios.get(concertQueryURL)
+    // console.log("concert URL: " + concertQueryURL);
+    axios
+    .get(concertQueryURL)
         .then(
             function (response) {
-                // console.log(JSON.stringify(response.data, null, 4));
                 let res = response.data;
                 for (var i = 0; i < res.length; i++) {
                     console.log(`
                 //     =======================
-                //     Artist: ${res[i].lineup[0]}
-                //     Venue Name: ${res[i].venue.name}
-                //     Location: ${res[i].venue.city}, ${res[i].venue.region}  ${res[i].venue.country}
-                //     Date: ${moment(res[i].datetime).format("MM/DD/YY")}
+                //     ARTIST: ${res[i].lineup[0]}
+                //     VENUE NAME: ${res[i].venue.name}
+                //     LOCATION: ${res[i].venue.city}, ${res[i].venue.region}  ${res[i].venue.country}
+                //     DATE: ${moment(res[i].datetime).format("MM/DD/YY")}
                 `)
                 }
             })
         .catch(function (error) {
-            console.log(error);
+            console.log("Band Name Not Found: " + error);
         });
 };
 
-switch (userInput) {
-    case "concert-this":
-        bandsInTownSearch(userSearchVal);
-        break;
-    // case "spotify-this-song": 
 
+// node liri.js spotify-this-song '<song name here>'
+// - This will show the following information about the song in your terminal/bash window
+// - Artist(s)
+// - The song's name
+// - A preview link of the song from Spotify
+// - The album that the song is from
+// - If no song is provided then your program will default to "The Sign" by Ace of Base.
 
+const spotifySearch = (song) => {
+    if(!song){ 
+        song = "The Sign, Ace of Base"; //Default search param in case no song is specified
+    }
+    spotify
+    .search({ type: 'track', query: song, limit: 8 })
+    .then(function(response) {
+        let res = response.tracks.items;
+        for (let i = 0; i < res.length; i++){
+            console.log(`
+            =======================
+            NAME: ${res[i].artists[0].name}
+            SONG TITLE: ${res[i].name}
+            PREVIEW LINK: ${res[i].external_urls.spotify}
+            ALBUM TITLE: ${res[i].album.name}
+            `)
 
-
+        }
+    })
+    .catch(function(error) {
+        console.log("Song or Artist Not Found. Try searching like this:   Song Title, Artist Name   :" + error);
+    })
 };
+
+
+// node liri.js movie-this '<movie name here>'
+//   * Title of the movie.
+//   * Year the movie came out.
+//   * IMDB Rating of the movie.
+//   * Rotten Tomatoes Rating of the movie.
+//   * Country where the movie was produced.
+//   * Language of the movie.
+//   * Plot of the movie.
+//   * Actors in the movie.
+
+const OMDBsearch = (movieInfo) => {
+    if(!movieInfo){
+        movieInfo = "Mr. Nobody"; //Default search param in case no movie is specified
+    }
+    let movieQueryURL = "http://www.omdbapi.com/?t=" + movieInfo + "&apikey=trilogy";
+    axios
+    .get(movieQueryURL)
+    .then(function(response){
+        let res = response.data;
+        // console.log(response.data);
+        console.log(`
+        ===========================
+        TITLE: ${res.Title}
+        YEAR: ${res.Year}
+        RATINGS: ${res.Ratings[0].Source}: ${res.Ratings[0].Value}, ${res.Ratings[1].Source}: ${res.Ratings[1].Value}
+        COUNTRY: ${res.Country}
+        LANGUAGE: ${res.Language}
+        PLOT: ${res.Plot}
+        ACTORS: ${res.Actors}
+        `)
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
+    
+
+
+// node liri.js do-what-it-says
+// - Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+// - It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+// - Edit the text in random.txt to test out the feature for movie-this and concert-this.
+const doIt = () => {
+    fs.readFile("random.txt", "utf8", function(err, data){
+        if(err){
+            console.log(err)
+        };
+    // console.log(data);
+    var dataArr = data.split(",");
+    // console.log(dataArr);
+    let userInput = dataArr[0];
+    let userSearchVal = dataArr[1];
+    // commandLog(userInput + ", " + userSearchVal);
+    processInputs(userInput, userSearchVal);
+    });
+};
+
+
+
+
+function commandLog (value){
+    fs.appendFile("log.txt", " \n " + value, function(err) {
+        if(err){
+            console.log(err)
+        }
+        console.log("Logged: " + value);
+    })
+};
+
+
+//Where the magic begins.... aka, here's how LIRI knows which route to take with the info User provides.
+function processInputs(userInput, userSearchVal) {
+    switch (userInput) {
+        case "concert-this":
+            bandsInTownSearch(userSearchVal);
+            commandLog(userInput + ", " + userSearchVal);
+            break;
+        case "spotify-this-song": 
+            spotifySearch(userSearchVal);
+            commandLog(userInput + ", " + userSearchVal);
+            break;
+        case "movie-this":
+            OMDBsearch(userSearchVal);
+            commandLog(userInput + ", " + userSearchVal);
+            break;
+        case "do-what-it-says":
+            doIt();
+            break;
+        default: 
+        console.log("Oops! Something went wrong! Try your search again.");
+    };
+};
+
+
+//Let's get this party started.....
+processInputs(userInput, userSearchVal);
---
+++ main.js
@@ -0,0 +1,287 @@
+[
+    {"offers": [
+            {
+                "type": "Tickets",
+                "url": "https://www.bandsintown.com/t/100785911?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket",
+                "status": "available"
+            }
+        ],
+        "venue": {
+            "name": "Park Theater",
+            "country": "United States",
+            "region": "NV",
+            "city": "Las Vegas",
+            "latitude": "36.1041689",
+            "longitude": "-115.1744739"
+        },
+        "datetime": "2019-04-29T19:00:09",
+        "on_sale_datetime": "",
+        "description": "",
+        "lineup": [
+            "Bruno Mars"
+        ],
+        "id": "100785911",
+        "artist_id": "279327",
+        "url": "https://www.bandsintown.com/e/100785911?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event"
+    },
+    {
+        "offers": [
+            {
+                "type": "Tickets",
+                "url": "https://www.bandsintown.com/t/100785913?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket",
+                "status": "available"
+            }
+        ],
+        "venue": {
+            "name": "Park Theater",
+            "country": "United States",
+            "region": "NV",
+            "city": "Las Vegas",
+            "latitude": "36.1041689",
+            "longitude": "-115.1744739"
+        },
+        "datetime": "2019-04-30T19:00:46",
+        "on_sale_datetime": "",
+        "description": "",
+        "lineup": [
+            "Bruno Mars"
+        ],
+        "id": "100785913",
+        "artist_id": "279327",
+        "url": "https://www.bandsintown.com/e/100785913?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event"
+    },
+    {
+        "offers": [
+            {
+                "type": "Tickets",
+                "url": "https://www.bandsintown.com/t/100785916?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket",
+                "status": "available"
+            }
+        ],
+        "venue": {
+            "name": "Park Theater",
+            "country": "United States",
+            "region": "NV",
+            "city": "Las Vegas",
+            "latitude": "36.1041689",
+            "longitude": "-115.1744739"
+        },
+        "datetime": "2019-05-03T19:00:07",
+        "on_sale_datetime": "",
+        "description": "",
+        "lineup": [
+            "Bruno Mars"
+        ],
+        "id": "100785916",
+        "artist_id": "279327",
+        "url": "https://www.bandsintown.com/e/100785916?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event"
+    },
+    {
+        "offers": [
+            {
+                "type": "Tickets",
+                "url": "https://www.bandsintown.com/t/100785919?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket",
+                "status": "available"
+            }
+        ],
+        "venue": {
+            "name": "Park Theater",
+            "country": "United States",
+            "region": "NV",
+            "city": "Las Vegas",
+            "latitude": "36.1041689",
+            "longitude": "-115.1744739"
+        },
+        "datetime": "2019-05-04T19:00:30",
+        "on_sale_datetime": "",
+        "description": "",
+        "lineup": [
+            "Bruno Mars"
+        ],
+        "id": "100785919",
+        "artist_id": "279327",
+        "url": "https://www.bandsintown.com/e/100785919?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event"
+    },
+    {
+        "offers": [
+            {
+                "type": "Tickets",
+                "url": "https://www.bandsintown.com/t/100785928?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket",
+                "status": "available"
+            }
+        ],
+        "venue": {
+            "name": "Park Theater",
+            "country": "United States",
+            "region": "NV",
+            "city": "Las Vegas",
+            "latitude": "36.1041689",
+            "longitude": "-115.1744739"
+        },
+        "datetime": "2019-09-03T19:00:53",
+        "on_sale_datetime": "",
+        "description": "",
+        "lineup": [
+            "Bruno Mars"
+        ],
+        "id": "100785928",
+        "artist_id": "279327",
+        "url": "https://www.bandsintown.com/e/100785928?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event"
+    },
+    {
+        "offers": [
+            {
+                "type": "Tickets",
+                "url": "https://www.bandsintown.com/t/100785930?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket",
+                "status": "available"
+            }
+        ],
+        "venue": {
+            "name": "Park Theater",
+            "country": "United States",
+            "region": "NV",
+            "city": "Las Vegas",
+            "latitude": "36.1041689",
+            "longitude": "-115.1744739"
+        },
+        "datetime": "2019-09-04T19:00:06",
+        "on_sale_datetime": "",
+        "description": "",
+        "lineup": [
+            "Bruno Mars"
+        ],
+        "id": "100785930",
+        "artist_id": "279327",
+        "url": "https://www.bandsintown.com/e/100785930?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event"
+    },
+    {
+        "offers": [
+            {
+                "type": "Tickets",
+                "url": "https://www.bandsintown.com/t/100785933?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket",
+                "status": "available"
+            }
+        ],
+        "venue": {
+            "name": "Park Theater",
+            "country": "United States",
+            "region": "NV",
+            "city": "Las Vegas",
+            "latitude": "36.1041689",
+            "longitude": "-115.1744739"
+        },
+        "datetime": "2019-09-07T19:00:32",
+        "on_sale_datetime": "",
+        "description": "",
+        "lineup": [
+            "Bruno Mars"
+        ],
+        "id": "100785933",
+        "artist_id": "279327",
+        "url": "https://www.bandsintown.com/e/100785933?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event"
+    },
+    {
+        "offers": [
+            {
+                "type": "Tickets",
+                "url": "https://www.bandsintown.com/t/100785939?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket",
+                "status": "available"
+            }
+        ],
+        "venue": {
+            "name": "Park Theater",
+            "country": "United States",
+            "region": "NV",
+            "city": "Las Vegas",
+            "latitude": "36.1041689",
+            "longitude": "-115.1744739"
+        },
+        "datetime": "2019-09-09T19:00:55",
+        "on_sale_datetime": "",
+        "description": "",
+        "lineup": [
+            "Bruno Mars"
+        ],
+        "id": "100785939",
+        "artist_id": "279327",
+        "url": "https://www.bandsintown.com/e/100785939?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event"
+    },
+    {
+        "offers": [
+            {
+                "type": "Tickets",
+                "url": "https://www.bandsintown.com/t/100785944?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket",
+                "status": "available"
+            }
+        ],
+        "venue": {
+            "name": "Park Theater",
+            "country": "United States",
+            "region": "NV",
+            "city": "Las Vegas",
+            "latitude": "36.1041689",
+            "longitude": "-115.1744739"
+        },
+        "datetime": "2019-09-10T19:00:27",
+        "on_sale_datetime": "",
+        "description": "",
+        "lineup": [
+            "Bruno Mars"
+        ],
+        "id": "100785944",
+        "artist_id": "279327",
+        "url": "https://www.bandsintown.com/e/100785944?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event"
+    },
+    {
+        "offers": [
+            {
+                "type": "Tickets",
+                "url": "https://www.bandsintown.com/t/100785946?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket",
+                "status": "available"
+            }
+        ],
+        "venue": {
+            "name": "Park Theater",
+            "country": "United States",
+            "region": "NV",
+            "city": "Las Vegas",
+            "latitude": "36.1041689",
+            "longitude": "-115.1744739"
+        },
+        "datetime": "2019-09-13T19:00:48",
+        "on_sale_datetime": "",
+        "description": "",
+        "lineup": [
+            "Bruno Mars"
+        ],
+        "id": "100785946",
+        "artist_id": "279327",
+        "url": "https://www.bandsintown.com/e/100785946?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event"
+    },
+    {
+        "offers": [
+            {
+                "type": "Tickets",
+                "url": "https://www.bandsintown.com/t/100785949?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket",
+                "status": "available"
+            }
+        ],
+        "venue": {
+            "name": "Park Theater",
+            "country": "United States",
+            "region": "NV",
+            "city": "Las Vegas",
+            "latitude": "36.1041689",
+            "longitude": "-115.1744739"
+        },
+        "datetime": "2019-09-14T19:00:07",
+        "on_sale_datetime": "",
+        "description": "",
+        "lineup": [
+            "Bruno Mars"
+        ],
+        "id": "100785949",
+        "artist_id": "279327",
+        "url": "https://www.bandsintown.com/e/100785949?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event"
+    }
+]
